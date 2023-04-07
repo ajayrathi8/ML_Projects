18e457addfd26cfa54199bfffd455e3d645ece39
ajayrathi8 <rathiajay08@gmail.com>
2023-04-07 20:16:02 +0530

    update

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages,setup
+
+
+
+
+setup(
+name="ML_Project",
+version="0.0.1",
+author="Ajay Rathi"
+author_email="rathiajay08@gmail.com"
+packages=find_packages(),
+install_requires=['pandas','numpy','seaborn']
+
+
+
+
+)
